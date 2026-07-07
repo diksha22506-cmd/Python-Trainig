5feb2ad6543e07c4ce5f0fbd84502fd7d3b60621
diksha22506-cmd <diksha22506@gmail.com>
2026-07-07 11:13:42 +0530

    dictionary checker

---
+++ Lab Work/day-2/dict_searcher.py
@@ -0,0 +1,22 @@
+# Function to search student by roll number
+def search_student(student_dict, roll_no):
+    if roll_no in student_dict:
+        return student_dict[roll_no]
+    else:
+        return "Student Not Found"
+
+
+# Main Program
+students = {
+    101: "Aman",
+    102: "Priya",
+    103: "Rahul",
+    104: "Sneha",
+    105: "Karan"
+}
+
+roll_no = int(input("Enter Roll Number: "))
+
+result = search_student(students, roll_no)
+
+print("Result:", result)
